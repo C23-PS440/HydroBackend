e662e11a039a831ae38e1fcf990d9bb545b8ef68
jerryjohnsonn <mail.jerryjohnson@gmail.com>
2023-06-13 22:00:49 +0700

    Deleting service_account.json

--- util/database.js
+++ util/database.js
@@ -3,11 +3,11 @@ const Sequelize = require('sequelize');
 // Connection to local database
 const db = new Sequelize({
   dialect: 'mysql',
-  host: 'localhost',
+  host: '34.101.210.160',
   port: parseInt(process.env.DB_PORT || 3306),
   database: 'capstone-database',
   username: 'root',
-  password: '',
+  password: ',8<bUPJ@7$65nOTR',
 });
 
 module.exports = db;
