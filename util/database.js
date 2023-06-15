ac7bd79b9e8195de4836d6e8f644a69eccc66e4a
jerryjohnsonn <mail.jerryjohnson@gmail.com>
2023-06-15 19:46:25 +0700

    Final Commit
    Semua fitur telah selesai dikembangkan, dan ini repo final sebelum pengumpulan capstone

--- util/database.js
+++ util/database.js
@@ -3,11 +3,11 @@ const Sequelize = require('sequelize');
 // Connection to local database
 const db = new Sequelize({
   dialect: 'mysql',
-  host: '34.101.210.160',
+  host: process.env.HOST || 'localhost',
   port: parseInt(process.env.DB_PORT || 3306),
   database: 'capstone-database',
   username: 'root',
-  password: ',8<bUPJ@7$65nOTR',
+  password: process.env.DB_PASSWORD,
 });
 
 module.exports = db;
